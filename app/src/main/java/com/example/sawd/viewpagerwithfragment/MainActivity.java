4ac499253da15ac9d33e5103bd40f9c8f4eda3f6
SAWD <devsawd@gmail.com>
2018-07-01 15:40:06 +0900

    - add SawdFragment - add fragment_sawd - implement SawdFragmentAdapter - add view pager

--- app/src/main/java/com/example/sawd/viewpagerwithfragment/MainActivity.java
+++ app/src/main/java/com/example/sawd/viewpagerwithfragment/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.sawd.viewpagerwithfragment;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(new SawdFragmentAdatper(getSupportFragmentManager()));
+
+    }
+
+    class SawdFragmentAdatper extends FragmentPagerAdapter {
+
+        private ArrayList<Fragment> mFragment = new ArrayList<>();
+        private SawdFragmentAdatper(FragmentManager fm) {
+            super(fm);
+            for(int i=0; i<5; i++) {
+                mFragment.add(new SawdFragment());
+            }
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return mFragment.get(position);
+        }
+        @Override
+        public int getCount() {
+            return mFragment.size();
+        }
     }
 }
---
+++ app/src/main/java/com/example/sawd/viewpagerwithfragment/SawdFragment.java
@@ -0,0 +1,22 @@
+package com.example.sawd.viewpagerwithfragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sawd.viewpagerwithfragment.R;
+
+public class SawdFragment extends Fragment {
+    public SawdFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sawd, container, false);
+    }
+}
