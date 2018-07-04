ea99dc950a9fd9f9fc36d2dff7ef4c8b0afa7b0b
SAWD <devsawd@gmail.com>
2018-07-04 09:11:03 +0900

    - Souce arrangement - App Icon apply
    Signed-off-by: Yongtaek Kim <kyt@pntbiz.com>

--- app/src/main/java/com/example/sawd/viewpagerwithfragment/MainActivity.java
+++ app/src/main/java/com/example/sawd/viewpagerwithfragment/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.sawd.viewpagerwithfragment;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     int MAX_PAGE = 3;
--- app/src/main/java/com/example/sawd/viewpagerwithfragment/SawdFragment1.java
+++ app/src/main/java/com/example/sawd/viewpagerwithfragment/SawdFragment1.java
@@ -3,7 +3,6 @@ package com.example.sawd.viewpagerwithfragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
